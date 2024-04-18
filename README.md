445aac07418208e4649c94971d94ab86eeb05976
Umair Siddique <umairsiddique3171@gmail.com>
2024-04-18 13:30:20 +0500

    updated repo

--- README.md
+++ README.md
@@ -1,9 +1,41 @@
 # youtube_script_generator
 
+*Struggling with YouTube script ideas?*
 
-streamlit - used to build the app
-langchain - used to build llm workflow
-openai - needed to use openai gpt
-wikipedia - used to connect gpt to wikipedia
-chomadb - vector storage
-tiktoken - backend tokenizer for openai
+This web app uses AI to generate video transcripts based on your titles, helping you create engaging content and save time!
+
+*Just enter your video title and hit submit!*
+
+## App Preview
+https://github.com/umairsiddique3171/youtube-script-generator/assets/148565997/12de3ca3-eb90-4526-aa4f-24acc64ad8b3
+
+## App Usage
+To run the application locally, follow these steps:
+
+1. **Clone the repository:**
+   ```
+   git clone https://github.com/umairsiddique3171/youtube-script-generator.git
+   cd youtube-script-generator
+   ```
+2. **Create and activate a virtual environment:**
+   ```
+   python -m venv env
+   .\env\Scripts\activate
+   ```
+3. **Install the required dependencies:**
+
+   ```
+   pip install -r requirements.txt
+   ```
+4. **Run the application:**
+   ```
+   streamlit run app.py
+   ```
+5. **Open a web browser and navigate to the provided ``URL``.**
+
+## License
+
+This project is licensed under the [MIT License](https://github.com/umairsiddique3171/youtube-script-generator/blob/main/LICENSE).
+
+## Author 
+[@umairsiddique3171](https://github.com/umairsiddique3171)
--- app.py
+++ app.py
@@ -1,26 +1,42 @@
-# Importing dependencies
+# importing dependencies
+from dotenv import load_dotenv
+load_dotenv() # loading .env variable as environment variables in current session
+
 import os 
-import json
+from PIL import Image
 import streamlit as st
-from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
-# from langchain.chains import LLMChain, SequentialChain 
-from langchain.memory import ConversationBufferMemory
-# from langchain.utilities import WikipediaAPIWrapper 
+import google.generativeai as genai
+from utils import set_background
 
-with open('api_key.json','r') as f:
-    api_key = json.load(f)
+# configuration with api_key
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-os.environ['OPENAI_API_KEY'] = api_key.get('api_key')
+# func to load GeminiProVision and get response
+model = genai.GenerativeModel("gemini-pro")
+def get_gemini_response(prompt,input_prompt):
+    response = model.generate_content([prompt, input_prompt])
+    return response.text
+# initial streamlit app
+st.set_page_config(page_title="Youtube Script Generator")
+st.header("Youtube Video Script Generator")
 
+# set background
+set_background('background_img.jpg')
 
-# App Framework
-st.title("🦜️🔗 Youtube Script Generator")
-prompt = st.text_input("Please enter your video topic here")
+# inputs
+input_prompt = st.text_input("#### Input Video Title: ",key="input")
 
-# Llms 
-llm = ChatOpenAI(temperature=0.9)
+# submit button
+submit = st.button("Submit")
 
-if prompt:
-    response = llm.invoke(prompt)
+prompt = """
+I will enter a youtube video title and I want you to generate video transcript based on that title.
+"""
+
+if submit : 
+    response = get_gemini_response(prompt,input_prompt)
+    st.subheader("Response: ")
     st.write(response)
+
+
+                                            
